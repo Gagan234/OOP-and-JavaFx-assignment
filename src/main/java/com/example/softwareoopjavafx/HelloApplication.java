afda08f2f3ab6b89bd665abef61b7f17adfa1a54
Gagan <gagansapkota668@gmail.com>
2024-02-27 02:01:12 -0500

    Still working

--- src/main/java/com/example/softwareoopjavafx/HelloApplication.java
+++ src/main/java/com/example/softwareoopjavafx/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1000, 550);
-        stage.setTitle("Hello!");
+        stage.setTitle("FSC CSC325_Full Stack Project");
         stage.setScene(scene);
         stage.show();
     }
